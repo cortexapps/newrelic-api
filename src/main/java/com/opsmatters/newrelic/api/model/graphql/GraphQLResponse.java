f845ea8d0783afe64b202169fd1e4c2d08695721
Nikhil Unni <nikhil.unni3000@gmail.com>
2020-01-28 16:09:10 -0800

    Update GraphQL model

--- src/main/java/com/opsmatters/newrelic/api/model/graphql/GraphQLResponse.java
+++ src/main/java/com/opsmatters/newrelic/api/model/graphql/GraphQLResponse.java
@@ -1,5 +1,7 @@
 package com.opsmatters.newrelic.api.model.graphql;
 
+import java.util.List;
+
 /**
  * Represents Graph QL response data.
  *
@@ -23,9 +25,8 @@ public class GraphQLResponse {
 
     public class Actor {
         private Account account;
-
-        public Actor() {
-        }
+        private Entity entity;
+        private EntitySearch entitySearch;
 
         public Account getAccount() {
             return account;
@@ -34,13 +35,26 @@ public class GraphQLResponse {
         public void setAccount(Account account) {
             this.account = account;
         }
+
+        public Entity getEntity() {
+            return entity;
+        }
+
+        public void setEntity(Entity entity) {
+            this.entity = entity;
+        }
+
+        public EntitySearch getEntitySearch() {
+            return entitySearch;
+        }
+
+        public void setEntitySearch(EntitySearch entitySearch) {
+            this.entitySearch = entitySearch;
+        }
     }
 
     public class Account {
         private Nrql nrql;
-
-        public Account() {
-        }
 
         public Nrql getNrql() {
             return nrql;
@@ -54,16 +68,102 @@ public class GraphQLResponse {
     public class Nrql {
         private String embeddedChartUrl;
 
-        public Nrql()
-        {
-        }
-
         public String getEmbeddedChartUrl() {
             return embeddedChartUrl;
         }
 
         public void setEmbeddedChartUrl(String embeddedChartUrl) {
             this.embeddedChartUrl = embeddedChartUrl;
+        }
+    }
+
+    public class Entity {
+        private List<Relationship> relationships;
+
+        public List<Relationship> getRelationships() {
+            return relationships;
+        }
+
+        public void setRelationships(List<Relationship> relationships) {
+            this.relationships = relationships;
+        }
+    }
+
+    public class Relationship {
+        private Source source;
+        private Source target;
+
+        public Source getSource() {
+            return source;
+        }
+
+        public void setSource(Source source) {
+            this.source = source;
+        }
+
+        public Source getTarget() {
+            return target;
+        }
+
+        public void setTarget(Source target) {
+            this.target = target;
+        }
+    }
+
+    public class Source {
+        private EmbeddedEntity entity;
+
+        public EmbeddedEntity getEntity() {
+            return entity;
+        }
+
+        public void setEntity(EmbeddedEntity entity) {
+            this.entity = entity;
+        }
+    }
+
+    public class EmbeddedEntity {
+        private String name;
+        private String guid;
+
+        public String getGuid() {
+            return guid;
+        }
+
+        public void setGuid(String guid) {
+            this.guid = guid;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    public class EntitySearch {
+        private Results results;
+
+        public Results getResults() {
+            return results;
+        }
+
+        public void setResults(Results results) {
+            this.results = results;
+        }
+    }
+
+    public class Results {
+        private List<EmbeddedEntity> entities;
+
+        public List<EmbeddedEntity> getEntities() {
+            return entities;
+        }
+
+        public void setEntities(List<EmbeddedEntity> entities) {
+            this.entities = entities;
         }
     }
 }
